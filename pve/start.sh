447c0372c9d62d26c1d4771e0a8c5c203f1ab0b1
shdopeoplesn <shdopeoplesn@gmail.com>
2021-12-28 09:23:50 +0800

    更新1.18.1&更改領地Y軸限制為[-64, 320]

--- pve/start.sh
+++ pve/start.sh
@@ -1,2 +1,2 @@
-screen java -server -Xmx15G -XX:+UseZGC -jar pve_core.jar nogui
+screen java -server -Xmx15G -XX:+UseG1GC -jar pve_core.jar nogui
 
--- pve3/start.sh
+++ pve3/start.sh
@@ -1,1 +1,1 @@
-screen java -server -Xmx15G -XX:+UseZGC -jar pve3_core.jar nogui
+screen java -server -Xmx15G -jar pve3_core.jar nogui
